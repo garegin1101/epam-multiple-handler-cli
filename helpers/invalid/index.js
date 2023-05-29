9d354dcefd7012c54683b4f066a9dc02afc64fd7
Garegin <gareginmiqayelyan8@gmail.com>
2023-05-29 22:18:13 +0400

    logs instead of console.log-s

--- helpers/invalid/index.js
+++ helpers/invalid/index.js
@@ -1,6 +1,10 @@
+import { log } from '../logs/index.js';
+
 export default (arg) => {
-    if(arg instanceof Array) console.log(`Invalid command: Type "os" or ".exit"`) 
-    else if(arg === 1) console.log("Invalid input: Please add hendler, for example --cpus")
-    else if(arg === 2) console.log("Invalid input: Please don't repeat handlers")
-    else console.log(`Invalid handler: "${arg}" is not a handler`);
+
+    if(arg instanceof Array) log(`Invalid command: Type "os" or ".exit"`) 
+    else if(arg === 1) log("Invalid input: Please add hendler, for example --cpus")
+    else if(arg === 2) log("Invalid input: Please don't repeat handlers")
+    else log(`Invalid handler: "${arg}" is not a handler`);
+
 };
